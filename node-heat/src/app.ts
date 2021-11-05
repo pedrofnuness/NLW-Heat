7d377797a2c4c91ceb6d7c823643756e146460c0
Pedro Nunes <pedro.nunes@southsystem.com.br>
2021-11-05 10:57:39 -0300

    :sparkles: feat: Created callback signin route

--- node-heat/src/app.ts
+++ node-heat/src/app.ts
@@ -9,4 +9,10 @@ app.get("/github", (request, response) => {
   );
 });
 
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
+
 app.listen(4000, () => console.log(`🚀 Server is running on PORT 4000`));
