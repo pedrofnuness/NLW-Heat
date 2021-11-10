690a40109283257794593d9e82cf31583aa44862
Pedro Nunes <pedro.nunes@southsystem.com.br>
2021-11-10 11:37:59 -0300

    :sparkles: feat: created user find and creation funcionality at authenticateUserService

--- node-heat/src/services/AuthenticateUserService.ts
+++ node-heat/src/services/AuthenticateUserService.ts
@@ -9,6 +9,7 @@
  */
 
 import axios from "axios";
+import prismaClient from "../prisma";
 
 interface IAccessTokenResponse {
   access_token: string;
@@ -45,6 +46,25 @@ class AuthenticateUserService {
         },
       }
     );
+
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
 
     return response.data;
   }
