7124c658757c0d5719059a7ed264f04f77d6ed34
Pedro Nunes <pedro.nunes@southsystem.com.br>
2021-11-10 16:51:12 -0300

    configurated websocket

--- node-heat/src/app.ts
+++ node-heat/src/app.ts
@@ -1,8 +1,25 @@
 import "dotenv/config";
 import express from "express";
 import { router } from "./routes";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
 app.use(express.json());
 
 app.use(router);
@@ -19,4 +36,4 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code);
 });
 
-app.listen(4000, () => console.log(`🚀 Server is running on PORT 4000`));
+export { serverHttp, io };
