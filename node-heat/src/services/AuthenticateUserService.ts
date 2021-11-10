cdf88e2c6071cb24982e325c3eb8594b07eaa6a2
Pedro Nunes <pedro.nunes@southsystem.com.br>
2021-11-10 10:01:56 -0300

    :sparkles: feat: created authenticate user service and controller

---
+++ node-heat/src/services/AuthenticateUserService.ts
@@ -0,0 +1,43 @@
+/**
+ * Receber code(string)
+ * Recuperar o access_token no github
+ *Recuperar infos do user no github
+ * Verificar se o usuário existe no DB
+ * ---- SIM = Gera um token
+ * ---- NÃO = Cria no DB, gera um token
+ * Retornar o token com as infos do user
+ */
+
+import axios from "axios";
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+    const response = await axios.get("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`,
+      },
+    });
+
+    return response.data;
+  }
+}
+
+export { AuthenticateUserService };
